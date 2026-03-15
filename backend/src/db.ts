63aa2c776ccdb596cc23df4b3f87082993667df9
TatiParra21 <tatiparras21@gmail.com>
2026-03-15 15:45:38 -0500

    nice

---
+++ backend/src/db.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "@prisma/client/extension";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const pool = new Pool({connectionString: process.env.DATABASE_URL})
+const adapter = new PrismaPg(pool as any)
+export const prisma = new PrismaClient(adapter)
